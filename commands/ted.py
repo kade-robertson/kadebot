4114ba31b7841416f657834bb2da6e7dbce1920e
Kade <kade-robertson@users.noreply.github.com>
2019-04-26 10:01:39 -0400

    finish scheduler

--- commands/ted.py
+++ commands/ted.py
@@ -12,7 +12,7 @@ import os
 import requests
 import datetime
 from telegram import ParseMode
-from .basic import CommandBase, CommandInfo, bot_command
+from .basic import CommandBase, CommandInfo, CommandType, bot_command
 
 
 class Ted(CommandBase):
@@ -22,13 +22,13 @@ class Ted(CommandBase):
     def __init__(self, logger):
         super().__init__(logger)
         self.sched_chats = dict()
+        self.temp_upd = None
         self.to_register = [
-            CommandInfo("ted", self.execute_ted,
-                        "Displays information for a specific TED Talk."),
-            CommandInfo("tedr", self.execute_tedr,
-                        "Displays information for a random TED Talk."),
-            # CommandInfo("tedadd", self.execute_add, "Schedule a daily random TED Talk."),
-            # CommandInfo("teddel", self.execude_del, "Remove an existing schedule.")
+            CommandInfo("ted", self.execute_ted, "Displays information for a specific TED Talk."),
+            CommandInfo("tedr", self.execute_tedr, "Displays information for a random TED Talk."),
+            CommandInfo("tedadd", self.execute_add, "Schedule a daily random TED Talk."),
+            CommandInfo("teddel", self.execute_del, "Remove an existing schedule."),
+            CommandInfo("ted_random", self.setup_talks, "Show scheduled daily TED Talks.", _type=CommandType.Schedule)
         ]
 
     def get_help_msg(self, cmd):
@@ -56,6 +56,16 @@ class Ted(CommandBase):
             with open(self.regfile, 'w') as f:
                 f.write('\n'.join(' '.join(map(str, x))
                         for x in self.sched_chats.items()))
+    
+    def setup_talks(self, updater):
+        self.temp_upd = updater
+        if self.sched_chats is not None:
+            for key, hour in self.sched_chats.items():
+                updater.job_queue.run_daily(
+                    self.talk_handler,
+                    time = datetime.time(hour, 0, 0),
+                    context = key
+                )
 
     def get_talk(self, id_or_slug, bot, chatid):
         data = None
@@ -77,9 +87,53 @@ class Ted(CommandBase):
                              parse_mode = ParseMode.HTML,
                              text = message,
                              disable_notification = True)
+    
+    def talk_handler(self, bot, job):
+        try:
+            if self.sched_chats is None or not job.context in self.sched_chats.keys():
+                job.schedule_removal()
+                return
+            self.get_talk(None, bot, job.context)
+            self.logger.info("ted_random schedule completed successfully.")
+        except Exception as e:
+            self.logger.error(e)
+
     @bot_command
     def execute_ted(self, bot, update, args):
         self.get_talk(args[0], bot, update.message.chat_id)
     @bot_command
     def execute_tedr(self, bot, update, args):
         self.get_talk(None, bot, update.message.chat_id)
+    @bot_command
+    def execute_add(self, bot, update, args):
+        if len(args) != 1:
+            bot.send_message(chat_id = update.message.chat_id,
+                             text = "This doesn't seem like correct usage of /tedadd.",
+                             disable_notification = True)
+            return
+        if not args[0].isdigit() and 0 <= int(args[0]) <= 23:
+            bot.send_message(chat_id = update.message.chat_id,
+                             text = "This is not a valid hour (0 <= hour <= 23).",
+                             disable_notification = True)
+            return
+        if update.message.chat_id in self.sched_chats.keys():
+            bot.send_message(chat_id = update.message.chat_id,
+                             text = "This chat has daily TED talks scheduled already.",
+                             disable_notification = True)
+            return
+        self.sched_chats[update.message.chat_id] = int(args[0])
+        self.temp_upd.job_queue.run_daily(
+            self.talk_handler,
+            time = datetime.time(int(args[0]), 0, 0),
+            context = update.message.chat_id
+        )
+        bot.send_message(chat_id = update.message.chat_id,
+                         text = "Daily TED talk has been scheduled.",
+                         disable_notification = True)
+    @bot_command
+    def execute_del(self, bot, update, args):
+        if update.message.chat_id in self.sched_chats.keys():
+            del self.sched_chats[update.message.chat_id]
+            bot.send_message(chat_id = update.message.chat_id,
+                             text = "Daily TED talks have been disabled.",
+                             disable_notification = True)
