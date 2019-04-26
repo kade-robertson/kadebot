41a9f63fe5bcbcda12dd185457409395a8674435
Kade <kade-robertson@users.noreply.github.com>
2019-04-26 09:52:24 -0400

    fix bugs in Ted

--- commands/ted.py
+++ commands/ted.py
@@ -14,18 +14,23 @@ import datetime
 from telegram import ParseMode
 from .basic import CommandBase, CommandInfo, bot_command
 
-class Ted(commandBase):
+
+class Ted(CommandBase):
     name = 'Ted'
     safename = 'ted'
+
     def __init__(self, logger):
         super().__init__(logger)
         self.sched_chats = dict()
         self.to_register = [
-            CommandInfo("ted", self.execute_ted, "Displays information for a specific TED Talk."),
-            CommandInfo("tedr", self.execute_tedr, "Displays information for a random TED Talk."),
+            CommandInfo("ted", self.execute_ted,
+                        "Displays information for a specific TED Talk."),
+            CommandInfo("tedr", self.execute_tedr,
+                        "Displays information for a random TED Talk."),
             # CommandInfo("tedadd", self.execute_add, "Schedule a daily random TED Talk."),
             # CommandInfo("teddel", self.execude_del, "Remove an existing schedule.")
         ]
+
     def get_help_msg(self, cmd):
         if cmd == "ted":
             return "Call /ted with a talk ID or slug to get information."
@@ -35,6 +40,7 @@ class Ted(commandBase):
             return "Call /tedadd <hour> to schedule a random TED Talk to be posted."
         elif cmd == "teddel":
             return "Call /teddel to remove an existing schedule for the current chat."
+
     def load_config(self, confdict):
         self.regfile = confdict['datfile']
         if os.path.isfile(self.regfile):
@@ -42,12 +48,15 @@ class Ted(commandBase):
                 lines = [x.strip().split(' ') for x in f.readlines()]
                 for chat, hour in lines:
                     self.sched_chats[int(chat)] = int(hour)
+
     def on_exit(self):
         if os.path.isfile(self.regfile):
             os.remove(self.regfile)
         if self.sched_chats:
             with open(self.regfile, 'w') as f:
-                f.write('\n'.join(' '.join(map(str, x)) for x in self.sched_chats.items()))
+                f.write('\n'.join(' '.join(map(str, x))
+                        for x in self.sched_chats.items()))
+
     def get_talk(self, id_or_slug, bot, chatid):
         data = None
         if not id_or_slug:
@@ -58,12 +67,12 @@ class Ted(commandBase):
             data = requests.get('https://ted.kaderobertson.pw/slug/' + id_or_slug).json()
         if not data:
             raise Exception("Couldn't get a valid TED talk.")
-         else:
+        else:
             duration = data['talks'][0]['duration']
-            dmin, dsec = (duration / 60).zfill(2), (duration % 60).zfill(2)
-            message = "<b>{}</b>\n\n{}\n\n".format(data['name'], data['description'])
-            message += "Talk: {}\n\nLength: {}:{}\n\n".format(data['url'], dmin, dsec)
-            message += "Recorded on {}".format(atetime.datetime.utcfromtimestamp(data['recorded_date'])).strftime('%Y-%m-%d')
+            dmin, dsec = int(duration // 60), int(duration % 60)
+            message = "<a href=\"{}\">{}</a>\n\n{}\n\n".format(data['url'], data['name'], data['description'])
+            message += "Length: {:02d}:{:02d}\n".format(dmin, dsec)
+            message += "Recorded on {} at {}.".format(datetime.datetime.utcfromtimestamp(data['recorded_date']).strftime('%Y-%m-%d'), data['event'])
             bot.send_message(chat_id = chatid,
                              parse_mode = ParseMode.HTML,
                              text = message,
@@ -74,5 +83,3 @@ class Ted(commandBase):
     @bot_command
     def execute_tedr(self, bot, update, args):
         self.get_talk(None, bot, update.message.chat_id)
-        
-        
